f5e91f34ff81c97102b791d8680dc52e32071164
Yusuf Akinbami <purplescroll@gmail.com>
2023-06-04 10:17:26 +0400

    Delete ShopFinder directory

--- ShopFinder/registration.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Copyright © 2023 All rights reserved.
- * See COPYING.txt for license details.
- */
-use Magento\Framework\Component\ComponentRegistrar;
-
-ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Chalhoub_ShopFinder', __DIR__);
-
